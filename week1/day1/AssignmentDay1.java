7a2726e875106082f50f3786e8f4fff53d00d2dd
Sangeetha020899 <sangeethap8723@gmail.com>
2025-03-02 13:33:50 +0530

    Day1 assignment

---
+++ week1/day1/AssignmentDay1.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class AssignmentDay1 {
+	
+	public static void main(String[] args) {
+		
+		String Name= "Sangeetha";
+		int pincode=600073;
+		long phno=892232122;
+		byte age=25;
+		char bloodgroup='o';
+		float height=5.8f;
+		double weight=67.984565;
+		short houseno=7687;
+		System.out.println("My name is :" +Name+ "; " +"My Blood group is :" +bloodgroup+ "; " +"My age is:" +age+"; " + "My height and weight is " +height+ ":" +weight+ "; " +"My house no is :" +houseno+"; " + "My phno is :" +phno+ "; " +"My pincode is :" +pincode);
+		
+	}
+	
+
+}
